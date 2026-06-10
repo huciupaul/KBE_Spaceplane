d3a4dbf21cfe7025a0463b4de3c3c0e67c4fb426
ymafoutsis <yasminemafoutsis@gmail.com>
2026-06-10 17:13:58 +0200

    update

---
+++ ReportCode/Mass_breakdown.py
@@ -0,0 +1,47 @@
+import matplotlib.pyplot as plt
+
+
+def plot_mass_breakdown(vehicle):
+    """
+    Plot a pie chart of the vehicle mass breakdown.
+
+    Parameters
+    ----------
+    vehicle : Spaceplane
+    """
+
+    p = vehicle.propulsion
+    f = vehicle.fuselage
+
+    labels = [
+        "Fuselage",
+        "Wings",
+        "Avionics",
+        "Payload",
+        "Tank walls",
+        "Propellant",
+    ]
+
+    masses = [
+        f.fuselage_structural_mass,
+        p.wings_mass,
+        p.avionics_mass,
+        p.payload_mass,
+        p.tank_wall_mass,
+        p.propellant_mass,
+    ]
+
+    plt.figure(figsize=(8, 8))
+
+    plt.pie(
+        masses,
+        labels=labels,
+        autopct="%1.1f%%",
+        startangle=90,
+    )
+
+    plt.title("Spaceplane Mass Breakdown")
+
+    plt.axis("equal")
+
+    plt.show()
